1eb4e4686934861881e014cde0b765617863e9f6
superior <superiorshipet@gmail.com>
2026-07-17 03:43:38 +0300

    added the read or GET method call it as u want

--- main.py
+++ main.py
@@ -41,3 +41,14 @@ tasks = [
         "done": True
     }
 ]
+@app.get("/tasks/{task_id}", summary="Get task by ID")
+def get_task(task_id: int):
+
+    for task in tasks:
+        if task["id"] == task_id:
+            return task
+
+    raise HTTPException(
+        status_code=404,
+        detail=f"Task {task_id} not found"
+    )
